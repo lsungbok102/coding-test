e060e2ac6e760abe2ebeb9bdcc637e0abd86e477
lsungbok102 <lsungbok102@gmail.com>
2025-03-31 17:50:16 +0900

    0331

--- java_basic/src/Main.java
+++ java_basic/src/Main.java
@@ -1,15 +1,18 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-public class Main {
-    public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+import java.util.Arrays;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+class Solution {
+    public String solution(String rsp) {
+        String
+        String answer = "";
+        int[] a = new int[rsp.length()];
+        int[] b = new int[rsp.length()];
+        for(int i = 0; i<rsp.length(); i++){
+            a[i] = rsp.charAt(i);
+            if(a[i]==0) b[i]=5;
+            else if(a[i]==2) b[i]=0;
+            else if(a[i]==5) b[i]=2;
         }
+        answer = Arrays.toString(b);
+        return answer;
     }
 }
